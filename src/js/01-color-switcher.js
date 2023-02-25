b42e3eb00b669fb82b0b066ab076ddd602e9085c
kirhal <kirillgaleckiy@gmail.com>
2023-02-25 15:46:55 +0200

    1-st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body');
+const startButtonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
+let bodyHexColor = null;
 
+startButtonEl.addEventListener('click', onStartBtnClick);
+stopButtonEl.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(e) {
+  startButtonEl.setAttribute('disabled', true);
+  stopButtonEl.removeAttribute('disabled');
+  bodyHexColor = setInterval(() => {
+    bodyEl.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick(e) {
+  stopButtonEl.setAttribute('disabled', true);
+  startButtonEl.removeAttribute('disabled');
+  clearInterval(bodyHexColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
